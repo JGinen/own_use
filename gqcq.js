b7fa2c21dffed88fd3839cee893fcea5752431d2
JGinen <63006287+JGinen@users.noreply.github.com>
2023-06-26 19:18:28 +0800

    Update gqcq.js

--- gqcq.js
+++ gqcq.js
@@ -4,6 +4,9 @@ var headers = $request.headers;
 
 if (url.indexOf("gacmotor.com") !== -1 && headers.hasOwnProperty("token")) {
     headers["token"] = token;
+    headers["deviceType"] = "iPhone";
+    headers["platformNo"] = "iOS";
+    headers["imei"] = "EE952283-6B8D-4966-A0FA-3C531D86D296";
 }
 
 $done({ headers: headers });
