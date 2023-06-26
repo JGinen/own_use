bc88960c4719f336d3950a9024edbb2361946b76
JGinen <63006287+JGinen@users.noreply.github.com>
2023-06-26 18:19:30 +0800

    Update gqcq.js

--- gqcq.js
+++ gqcq.js
@@ -2,7 +2,7 @@ var token = "0b2afa176c0643f4978ac14eec0afa0f";
 var url = $request.url;
 var headers = $request.headers;
 
-if (url.indexOf("gsp.gacmotor.com") !== -1 && headers.hasOwnProperty("token")) {
+if (url.indexOf("gacmotor.com") !== -1 && headers.hasOwnProperty("token")) {
     headers["token"] = token;
 }
 
