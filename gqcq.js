934d1cdc642c8a1345443283697e8ec82d07c52b
JGinen <63006287+JGinen@users.noreply.github.com>
2023-06-26 19:32:09 +0800

    Update gqcq.js

--- gqcq.js
+++ gqcq.js
@@ -2,11 +2,8 @@ var token = "5fca6fd1ef7d4b2691a5c22accad076a";
 var url = $request.url;
 var headers = $request.headers;
 
-if (url.indexOf("gacmotor.com") !== -1 {
+if (url.indexOf("gacmotor.com") !== -1 && headers.hasOwnProperty("token")) {
     headers["token"] = token;
-    headers["deviceType"] = "iPhone";
-    headers["platformNo"] = "iOS";
-    headers["imei"] = "EE952283-6B8D-4966-A0FA-3C531D86D296";
 }
 
 $done({ headers: headers });
