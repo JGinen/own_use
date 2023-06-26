6206ee9c0c6d4510f942e38b119866fae7cf6581
JGinen <63006287+JGinen@users.noreply.github.com>
2023-06-26 17:53:23 +0800

    Create gqcq.js

---
+++ gqcq.js
@@ -0,0 +1,9 @@
+var token = "0b2afa176c0643f4978ac14eec0afa0f";
+var url = $request.url;
+var headers = $request.headers;
+
+if (url.indexOf("gsp.gacmotor.com") !== -1 && headers.hasOwnProperty("token")) {
+    headers["token"] = token;
+}
+
+$done({ headers: headers });
