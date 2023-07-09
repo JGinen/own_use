05ca90d8e0e5ef4d1fca4554b121d8581e6e2535
JGinen <63006287+JGinen@users.noreply.github.com>
2023-07-09 19:38:09 +0800

    Update dd_elecV2P_quickStudy.js

--- dd_elecV2P_quickStudy.js
+++ dd_elecV2P_quickStudy.js
@@ -1,14 +1,3 @@
-const urlPattern = /^https:\/\/tmsh5\.faw-vw\.com\/gateway-elearning\/api-course\/v1\/mobile\/courses\/\d+\/\d+\/record/;
-const modifiedUrl = $request.url.replace(/\d+\?/, "8888?");
-
-if (urlPattern.test($request.url)) {
-    $task.fetch({
-        url: modifiedUrl
-    }).then(response => {
-        $done({ response });
-    }).catch(error => {
-        $done({});
-    });
-} else {
-    $done({});
-}
+const url = $request.url;
+const modifiedUrl = url.replace(/\/1(\?isOver=false&pause=true)$/, '/8888$1');
+$done({ url: modifiedUrl });
