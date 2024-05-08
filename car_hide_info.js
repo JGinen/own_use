ce011acce3eabf30f2a3428c89cfdde0a3900b6f
JGinen <63006287+JGinen@users.noreply.github.com>
2024-05-08 10:39:25 +0800

    Update car_hide_info.js

--- car_hide_info.js
+++ car_hide_info.js
@@ -2,13 +2,13 @@ let url = $request.url;
 let body = $response.body;
 
 let regexTime = /"time":\d+,/;
-let regexMobile = /"mobile":"\d+"/;
+let regexMobile = /"mobile":"\d+******\d+"/;
 let regexCompanyName = /"companyName":"[^"]+"/;
 let regexName = /"name":"[^"]+"/;
 
-body = body.replace(regexTime, '"time": "",');
-body = body.replace(regexMobile, '"mobile": ""');
-body = body.replace(regexCompanyName, '"companyName": ""');
-body = body.replace(regexName, '"name": ""');
+body = body.replace(regexTime, '"time": "*",');
+body = body.replace(regexMobile, '"mobile": "*"');
+body = body.replace(regexCompanyName, '"companyName": "*"');
+body = body.replace(regexName, '"name": "*"');
 
 $done({ body });
