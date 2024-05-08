c48ab356aba75911ed2e8eea92fae5e5bdfaecf5
JGinen <63006287+JGinen@users.noreply.github.com>
2024-05-08 10:22:56 +0800

    Create car_hide_info.js

---
+++ car_hide_info.js
@@ -0,0 +1,14 @@
+let url = $request.url;
+let body = $response.body;
+
+let regexTime = /"time":\d+,/;
+let regexMobile = /"mobile":"\d+"/;
+let regexCompanyName = /"companyName":"[^"]+"/;
+let regexName = /"name":"[^"]+"/;
+
+body = body.replace(regexTime, '"time": "",');
+body = body.replace(regexMobile, '"mobile": ""');
+body = body.replace(regexCompanyName, '"companyName": ""');
+body = body.replace(regexName, '"name": ""');
+
+$done({ body });
